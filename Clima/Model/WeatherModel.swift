8ebfb2ff3893f593e705a72cb8307886191ddec6
michael-s-wu <57376088+michael-s-wu@users.noreply.github.com>
2021-06-21 09:13:25 -0700

    Add files via upload

---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,38 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Michael Wu on 4/8/20.
+//  Copyright © 2020 Michael Wu. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 700...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        default:
+            return "cloud"
+        }
+    }
+    
+}
